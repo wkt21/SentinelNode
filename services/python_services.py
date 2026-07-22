4eae009e31664e151b1cec87b74ea31aa9b3c1aa
Frank Francis <admins.goog@gmail.com>
2026-07-22 10:41:45 -0700

    Create python_services.py

---
+++ services/python_services.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import socket
+
+HOST = "0.0.0.0"
+PORT = 8822
+
+while True:
+    s = socket.socket()
+    s.bind((HOST, PORT))
+    s.listen(1)
+    conn, addr = s.accept()
+    conn.send(b"AI‑Defense Layer 1\n")
+    conn.close()
